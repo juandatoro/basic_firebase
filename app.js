14ed4bbc285e036053b58a28e64e62155ea8afa1
Juan David Restrepo <juan.restrepotoro@gmail.com>
2018-03-08 19:07:23 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const $inputName = document.getElementById('name');
+const $inputEmail = document.getElementById('email');
+const $inputAge = document.getElementById('age');
+
+
+function saveUser(evt) {
+  evt.preventDefault();
+  const name = inputName.value;
+
+  const row = `
+    <
+  `
+}
